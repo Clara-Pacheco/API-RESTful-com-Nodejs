d89e3945051fb24b317ec7342f63dd265acf3fc2
Clara <clarapachecodev@gmail.com>
2023-07-06 10:30:29 -0300

    Create const destructuring method and url from req and test through conditionals if it's a GET or POST methods

--- src/server.js
+++ src/server.js
@@ -1,6 +1,18 @@
 import http from 'node:http'
 
 const server = http.createServer((req,res)=>{
+  const { method, url } = req
+  console.log(method, url)
+
+  if(method === 'GET' && url === '/users'){
+    //Early return
+      return res.end('Listagem de usuários')
+  }
+
+  if(method === 'POST' && url === '/users'){
+    return res.end('Criação de usuário')
+  }
+  
   return res.end('Hello Clara')
 })
 
