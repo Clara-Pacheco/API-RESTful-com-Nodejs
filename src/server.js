b4337e9eef436305e910873e7b3eaa174ce08a52
Clara <clarapachecodev@gmail.com>
2023-07-15 12:18:49 -0300

    Create a static user with POST method and read it with GET + setting a Header + passing status code for each server response

--- src/server.js
+++ src/server.js
@@ -1,19 +1,26 @@
 import http from 'node:http'
 
+const users = []
+
 const server = http.createServer((req,res)=>{
   const { method, url } = req
   console.log(method, url)
 
   if(method === 'GET' && url === '/users'){
-    //Early return
-      return res.end('Listagem de usuários')
+      return res
+      .setHeader('Content-type','application/json')
+      .end(JSON.stringify(users))
   }
 
   if(method === 'POST' && url === '/users'){
-    return res.end('Criação de usuário')
+    users.push({
+      id:1,
+      name:'Clara Pacheco',
+      email: 'clarap@gmail.com',
+    })
+    return res.writeHead(201).end()
   }
-  
-  return res.end('Hello Clara')
+  return res.writeHead(404).end()
 })
 
 
